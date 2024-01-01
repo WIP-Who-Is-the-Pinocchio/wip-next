3248f6a0218b2ca614f086d293a864fa3138c477
Yoonjae Choi <dbswo9795@gmail.com>
2024-01-01 16:54:22 +0900

    feat: 랭킹 컴포넌트 구현

---
+++ src/app/components/rank/Rank.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { twMerge } from 'tailwind-merge';
+import Image from 'next/image';
+
+interface RankProps {}
+
+const RANDOM_IMAGE = 'https://picsum.photos/102';
+
+const twProfile = 'flex flex-col items-center';
+
+export default function Rank({}: RankProps) {
+  return (
+    <section className="flex flex-col items-center">
+      <div className="mb-[26px]">{'(전국) 공약 이행률 가장 낮은 3인'}</div>
+      <article className="flex items-end gap-[40px]">
+        <div className={twMerge(twProfile)}>
+          <span className="mb-[6px] text-[14px] text-[#8C8C8C]">2%</span>
+          <Image
+            className="rounded-full border-[1px] border-solid border-[#F1F1F1]"
+            src={RANDOM_IMAGE}
+            alt=""
+            width={74}
+            height={74}
+          />
+          <span className="mb-[8px] mt-[14px] text-[16px] font-semibold leading-[100%] text-black">
+            김OO
+          </span>
+          <span className="text-[14px] font-semibold leading-[100%] text-[#8C8C8C]">
+            마포구을
+          </span>
+        </div>
+        <div className={twMerge(twProfile)}>
+          <span className="mb-[8px text-[14px] text-[#8C8C8C]">2%</span>
+          <Image
+            className="rounded-full border-[1px] border-solid border-[#F1F1F1]"
+            src={RANDOM_IMAGE}
+            alt=""
+            width={104}
+            height={104}
+          />
+          <span className="mb-[8px] mt-[14px] text-[16px] font-semibold leading-[100%] text-black">
+            김OO
+          </span>
+          <span className="text-[14px] font-semibold leading-[100%] text-[#8C8C8C]">
+            마포구을
+          </span>
+        </div>
+        <div className={twMerge(twProfile)}>
+          <span className="mb-[6px] text-[14px] text-[#8C8C8C]">2%</span>
+          <Image
+            className="rounded-full border-[1px] border-solid border-[#F1F1F1]"
+            src={RANDOM_IMAGE}
+            alt=""
+            width={74}
+            height={74}
+          />
+          <span className="mb-[8px] mt-[14px] text-[16px] font-semibold leading-[100%] text-black">
+            김OO
+          </span>
+          <span className="text-[14px] font-semibold leading-[100%] text-[#8C8C8C]">
+            마포구을
+          </span>
+        </div>
+      </article>
+    </section>
+  );
+}
