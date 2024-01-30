48de62375805ddb41c1a7d3ffb36c5900b467ffa
Yoonjae Choi <dbswo9795@gmail.com>
2024-01-30 23:53:17 +0900

    fix: 테이블 데이터 타입 수정 및 UI 수정

--- src/app/components/congress/Table.tsx
+++ src/app/components/congress/Table.tsx
@@ -6,7 +6,7 @@ export type TableHeader = string;
 // 테이블 행 데이터 타입 정의
 export interface TableRowData {
   category: string;
-  details: string[];
+  details: (string | number | null | undefined)[];
 }
 
 // 테이블 컴포넌트의 props 타입 정의
@@ -26,7 +26,7 @@ export const Table = ({ headers, data }: TableProps) => {
             {headers.map((header) => (
               <th
                 key={header}
-                className="border border-[#EDEDED] bg-[#F6F6F6] py-2.5 font-medium text-[#6D6D6D]"
+                className="border border-[#EDEDED] bg-[#F6F6F6] px-1 py-2.5 font-medium text-[#6D6D6D]"
               >
                 {header}
               </th>
@@ -36,13 +36,13 @@ export const Table = ({ headers, data }: TableProps) => {
         <tbody>
           {data.map((row, rowIndex) => (
             <tr key={rowIndex}>
-              <td className="border border-[#EDEDED] text-center">
+              <td className="border border-[#EDEDED] px-1 py-2.5 text-center">
                 {row.category}
               </td>
               {row.details.map((detail, detailIndex) => (
                 <td
                   key={detailIndex}
-                  className="border border-[#EDEDED] px-4 py-2 text-center"
+                  className="border border-[#EDEDED] text-center"
                 >
                   {detail}
                 </td>
