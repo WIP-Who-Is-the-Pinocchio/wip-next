9ade8a9dac73063f61eb43b3373cc649ae74072b
bible <mongry999@gmail.com>
2024-01-31 22:03:47 +0900

    chore: list page 루트 경로로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,144 @@
+'use client';
+
+import { useState } from 'react';
+import type { ChangeEventHandler } from 'react';
 import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
 
-export default function Home() {
+import Rank from './components/rank/Rank';
+import Tabs, { Tab } from './components/list/Tabs';
+import Regions from './components/list/Regions';
+import SEO from './components/SEO';
+import { MPBox } from './components/congress/MPBox';
+import { DUMMY_DATA, MPDataType } from '../api/api';
+
+const tabs: Tab[] = ['COUNTRYWIDE', 'REGION', 'PARTY'];
+
+export default function List() {
+  const [selectedTab, setSelectedTab] = useState<Tab>('COUNTRYWIDE');
+  const [search, setSearch] = useState('');
+
+  const sortData = DUMMY_DATA.sort((a, b) => {
+    const ratioA =
+      a.base_info.total_promise_count > 0
+        ? a.base_info.completed_promise_count / a.base_info.total_promise_count
+        : 0;
+    const ratioB =
+      b.base_info.total_promise_count > 0
+        ? b.base_info.completed_promise_count / b.base_info.total_promise_count
+        : 0;
+
+    return ratioB - ratioA; // 내림차순 정렬
+  });
+
+  const handleSelectTab = (tab: Tab) => () => {
+    setSelectedTab(tab);
+  };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log(`검색어: ${search}`);
+  };
+
+  const getLowestPromiseCount = (data: MPDataType[]) => {
+    // 비율 계산하여 데이터에 추가
+    const dataWithRatio = data.map((item: MPDataType) => {
+      const { completed_promise_count, total_promise_count } = item.base_info;
+      const ratio = (completed_promise_count / total_promise_count || 0) * 100;
+      const roundedRatio = ratio.toFixed(2);
+      return { ...item, roundedRatio };
+    });
+    // console.log(dataWithRatio);
+
+    // 비율을 기준으로 데이터 정렬
+    const sortedData = dataWithRatio.sort(
+      (a: any, b: any) => a.roundedRatio - b.roundedRatio
+    );
+
+    // 가장 낮은 3명의 데이터 선택
+    const lowestThree = sortedData.slice(0, 3);
+
+    return lowestThree;
+  };
+
+  const lowestThreePromises = getLowestPromiseCount(DUMMY_DATA);
+  // console.log(lowestThreePromises);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <SEO title="국회의원 공약이행률 순위" />
+      <section className="flex flex-col items-center py-[30px]">
+        <div className="px-[20px] text-[16px] font-bold leading-[150%] text-black">
+          WIP
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <article className="mb-[40px] mt-[10px] px-[20px]">
+          <p className="text-[14px] font-normal not-italic leading-[150%] text-black">
+            누가 피노키오인가?
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        </article>
+        <article className="mb-[40px] px-[30px]">
+          <Rank data={lowestThreePromises} />
+        </article>
+        <article
+          style={{ boxShadow: '0px 0px 16px 0px #E6E6E6' }}
+          className="mt-[11px] flex w-full flex-col items-center gap-[20px] rounded-t-[36px] px-[20px] pt-[24px]"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Tabs
+            tabs={tabs}
+            selectedTab={selectedTab}
+            onSelectTab={handleSelectTab}
+          />
+          <div
+            className="border-b-solid relative mb-[22px] mt-[4px] w-full border-b-[2px] border-b-[#f1f1f1] py-[12px]"
+            onClick={handleSearch}
+          >
+            <input
+              className={twMerge(
+                'w-full pr-[34px] text-[18px] leading-[100%] text-black outline-none',
+                'placeholder:text-[18px] placeholder:font-normal placeholder:not-italic placeholder:leading-[100%] placeholder:text-[#C1C1C1]'
+              )}
+              value={search}
+              type="text"
+              placeholder="이름으로 검색하기"
+              onChange={handleChange}
+            />
+            <Image
+              className="absolute right-0 top-[12px] hover:cursor-pointer"
+              src="/search_icon.svg"
+              width={18}
+              height={18}
+              alt="검색 버튼 아이콘"
+            />
+          </div>
+          {/* {selectedTab === 'COUNTRYWIDE' ? (
+          <>
+            {polifakeData.map((item:any) => (
+              <Card data={item} key={item.id} />
+            ))}
+          </>
+        ) : (
+          <Regions />
+        )} */}
+          {selectedTab === 'COUNTRYWIDE' ? (
+            <>
+              {sortData.map((mpData: MPDataType, index) => (
+                <MPBox
+                  key={`${mpData.base_info.name}-${index}`}
+                  mpData={mpData}
+                  ranking={index}
+                />
+              ))}
+            </>
+          ) : selectedTab === 'REGION' ? (
+            <Regions />
+          ) : (
+            <div className="h-[500px]">Coming soon</div>
+          )}
+        </article>
+      </section>
+    </>
   );
 }
