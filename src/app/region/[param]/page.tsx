3da79a45331062dfb877973b98c0c4b7a834faa6
ksknote <mongry999@gmail.com>
2024-02-04 17:30:06 +0900

    feat: 지역별 국회의원 리스트 컴포넌트 구현

--- src/app/region/[param]/page.tsx
+++ src/app/region/[param]/page.tsx
@@ -2,6 +2,8 @@
 
 import SEO from '@/app/components/SEO';
 import { useState } from 'react';
+import MPListContainer from '@/app/components/congress/MPListContainer';
+import { DUMMY_DATA, MPDataType } from '@/api/api';
 
 interface paramsType {
 param: string;
@@ -14,12 +16,24 @@ const RegionPage = ({params}:{params: paramsType} ) => {
     district: param[1],
   });
 
+  const sortRegionMPData = (mpData:MPDataType[]) => {
+    return mpData.filter((data: MPDataType)=> {
+      if (data.constituency[0].region !== constituency.region) {
+        return false;
+      }
+
+      if (constituency.district) {
+        return data.constituency.some(obj => obj.district ===constituency.district)
+      }
+      
+      return true
+    })
+  }
+
   return (
     <>
       <SEO title={`${constituency.region} 지역 국회의원 공약이행률 순위`} />
-      <div>
-        <h2>Region: {constituency.region}, District: {constituency.district}</h2>
-      </div>
+      <MPListContainer mpData={sortRegionMPData(DUMMY_DATA)} />
     </>
   );
 };
