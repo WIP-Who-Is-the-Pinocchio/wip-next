260a217ea482ddf37820ba86b5fbe55f7d59997d
bible <mongry999@gmail.com>
2024-02-05 22:42:55 +0900

    feat: 모든 페이지 seo용 header 컴포넌트 삽입

--- src/app/party/page.tsx
+++ src/app/party/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import SEO from '../components/SEO';
 import Tabs from '@/app/components/list/Tabs';
 import Party from '../components/list/Party';
 import MainLayout from '../components/common/MainLayout';
 
 export default function PartyPage() {
   return (
-    <MainLayout>
-      <Tabs selectedTab={'PARTY'} />
-      <Party />
-    </MainLayout>
+    <>
+      <SEO title="국회의원 정당 리스트" />
+      <MainLayout>
+        <Tabs selectedTab={'PARTY'} />
+        <Party />
+      </MainLayout>
+    </>
   );
 }
--- src/app/region/page.tsx
+++ src/app/region/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import SEO from '../components/SEO';
 import Tabs from '@/app/components/list/Tabs';
 import { Region } from '../components/list/Region';
 import MainLayout from '../components/common/MainLayout';
 
 export default function RegionPage() {
   return (
-    <MainLayout>
-      <Tabs selectedTab={'REGION'} />
-      <Region />
-    </MainLayout>
+    <>
+      <SEO title="국회의원 선거구 리스트" />
+      <MainLayout>
+        <Tabs selectedTab={'REGION'} />
+        <Region />
+      </MainLayout>
+    </>
   );
 }
