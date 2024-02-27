e8077fa6182976233bc4aea3f137adcdadaa2fd6
Yoonjae Choi <dbswo9795@gmail.com>
2024-02-27 23:57:38 +0900

    fix: 구글 콘솔 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Head from 'next/head';
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -37,6 +38,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="_XvjMrZ8YecRaEydlePwRJE_fWBtRafTZZKh0OnXtok"
+        />
+      </Head>
       <body className="flex justify-center">{children}</body>
     </html>
   );
