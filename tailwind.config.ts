35a4d6770f453d882d86b6abb3066b3bd9d9100b
Yoonjae Choi <dbswo9795@gmail.com>
2024-03-20 00:03:33 +0900

    design: 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/constants/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
@@ -19,6 +20,9 @@ const config: Config = {
         'primary-text': '#9333EA',
         gray: '#F8F8F8',
         deepGray: '#848484',
+        minjoo: '#17288B',
+        kookmin: '#E60024',
+        justice: '#007C36',
       },
       boxShadow: {
         box: '0px 0px 4px 0px #E8E8E8',
