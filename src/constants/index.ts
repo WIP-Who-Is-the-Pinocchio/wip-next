79a4cf02162bdbefa4f2b61be6715d0a6f746e69
Yoonjae Choi <dbswo9795@gmail.com>
2024-03-20 00:04:55 +0900

    feat: 당별 디자인 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export * from './styleClass';
+export * from './party';
---
+++ src/constants/party.ts
@@ -0,0 +1,23 @@
+export const PARTY_DATA: {
+  [key: string]: {
+    border: string;
+    logo: string;
+  };
+} = {
+  더불어민주당: {
+    border: 'border-minjoo',
+    logo: '/icons/minjoo.svg',
+  },
+  국민의힘: {
+    border: 'border-kookmin',
+    logo: '/icons/kookmin.svg',
+  },
+  녹색정의당: {
+    border: 'border-justice',
+    logo: 'icons/justice.svg',
+  },
+  무소속: {
+    border: 'border-[#F1F1F1]',
+    logo: '',
+  },
+};
