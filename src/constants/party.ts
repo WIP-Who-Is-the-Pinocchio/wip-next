7b9a0b792166effdfb043c03f9186a28d0a6b9a3
Yoonjae Choi <dbswo9795@gmail.com>
2024-03-21 00:19:40 +0900

    fix: 스타일 수정

--- src/constants/party.ts
+++ src/constants/party.ts
@@ -5,19 +5,19 @@ export const PARTY_DATA: {
   };
 } = {
   더불어민주당: {
-    border: 'border-minjoo',
+    border: '#17288B',
     logo: '/icons/minjoo.svg',
   },
   국민의힘: {
-    border: 'border-kookmin',
+    border: '#E60024',
     logo: '/icons/kookmin.svg',
   },
   녹색정의당: {
-    border: 'border-justice',
+    border: '#007C36',
     logo: 'icons/justice.svg',
   },
   무소속: {
-    border: 'border-[#F1F1F1]',
+    border: '#525252',
     logo: '',
   },
 };
