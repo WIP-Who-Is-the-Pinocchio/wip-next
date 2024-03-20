9a7dc4733d11a9e26ae09b462177a94253a2c447
Yoonjae Choi <dbswo9795@gmail.com>
2024-03-21 00:18:22 +0900

    fix: 탭 컴포넌트 수정

--- src/components/list/Tabs.tsx
+++ src/components/list/Tabs.tsx
@@ -1,66 +1,39 @@
 'use client';
 
-import { twMerge } from 'tailwind-merge';
-import { useRouter } from 'next/navigation';
+import { cn } from '@/utils';
+import { ListType, TabKey } from '@/constants';
 
 const twTab =
-  'py-[8px] px-[24px] rounded-[36px] text-[16px] not-italic leading-[100%] cursor-pointer';
-const twSelectedTab = 'text-[#9333EA] font-semibold bg-white border-black';
-const twUnselectedTab = 'text-[#C084FC] font-medium';
-
-const TabEnum = {
-  COUNTRYWIDE: 0,
-  REGION: 1,
-  PARTY: 2,
-} as const;
-
-export type Tab = keyof typeof TabEnum;
-
-const getTabText = (tab: Tab) => {
-  switch (tab) {
-    case 'COUNTRYWIDE':
-      return '전국';
-    case 'REGION':
-      return '지역별';
-    case 'PARTY':
-      return '정당별';
-    default:
-  }
-};
-
-const tabs: Tab[] = ['COUNTRYWIDE', 'REGION', 'PARTY'];
+  'py-2 px-6 rounded-[36px] text-[16px] not-italic leading-[100%] cursor-pointer';
+const twSelectedTab = 'text-black font-bold bg-white';
+const twUnselectedTab = 'text-[#909090] font-bold';
 
 interface TabsProps {
-  selectedTab: Tab;
+  tabList: ListType<TabKey>[];
+  selectedTab: TabKey;
+  handleSelectedTab: (key: string) => void;
 }
 
-export default function Tabs({ selectedTab }: TabsProps) {
-  const router = useRouter();
-  const handleSelectTab = (tab: Tab) => {
-    if (tab === 'COUNTRYWIDE') {
-      return router.push(`/`);
-    } else if (tab === 'REGION') {
-      return router.push('/region');
-    } else if (tab === 'PARTY') {
-      return router.push('/party');
-    }
-  };
-
+export default function Tabs({
+  tabList,
+  selectedTab,
+  handleSelectedTab,
+}: TabsProps) {
   return (
     <div
-      style={{ boxShadow: '0px 0px 4px 0px #C084FC7A inset' }}
-      className="flex w-fit gap-[10px] rounded-[36px] bg-[#F3E8FF] p-[3px]"
+      style={{ boxShadow: '0px 0px 4px 0px #B9B9B9 inset' }}
+      className="flex w-fit rounded-[36px] bg-[#F3F3F3] p-[3px]"
     >
-      {tabs.map((tab, i) => (
+      {tabList.map((tab, i) => (
         <div
           key={i.toString()}
-          className={twMerge(
+          className={cn(
             twTab,
-            selectedTab === tab ? twSelectedTab : twUnselectedTab
+            selectedTab === tab.key ? twSelectedTab : twUnselectedTab
           )}
-          onClick={() => handleSelectTab(tab)}
+          onClick={() => handleSelectedTab(tab.key)}
         >
-          {getTabText(tab)}
+          {tab.text}
         </div>
       ))}
     </div>
