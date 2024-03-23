ac1b14e5046c63a3210789b0642631ae6608e117
Yoonjae Choi <dbswo9795@gmail.com>
2024-03-23 14:10:37 +0900

    feat: 지역구, 정당별 컴포넌트로 변환 및 스텝 분리

--- src/components/list/Party.tsx
+++ src/components/list/Party.tsx
@@ -1,32 +1,62 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { partyData } from '@/api/partyData';
+import { useEffect, useState } from 'react';
 
-const Party = () => {
-  const router = useRouter();
+import { partyData } from '@/api/partyData';
+import { MPDataType } from '@/api/api';
+import MPListContainer from '../congress/MPListContainer';
+
+interface PartyComponentProps {
+  partyStep: number;
+  nextPartyStep: () => void;
+  mpData: MPDataType[];
+}
+
+export const PartyComponent = ({
+  partyStep,
+  nextPartyStep,
+  mpData,
+}: PartyComponentProps) => {
+  const [currentParty, setCurrentData] = useState<string | null>(null);
+  const [filteredMPData, setFilteredMPData] = useState(mpData);
 
   const handleRegionClick = (currentParty: string | null) => {
-    return router.push(`/party/${currentParty}`);
+    setCurrentData(currentParty);
+    nextPartyStep();
   };
 
+  useEffect(() => {
+    const filteredData = mpData.filter((data) => {
+      if (currentParty) {
+        return data.base_info.political_party.includes(currentParty);
+      }
+
+      return true;
+    });
+
+    setFilteredMPData(filteredData);
+  }, [currentParty, mpData]);
+
   return (
-    <section className="mb-[120px] flex w-full flex-col gap-16">
-      <div className="mb-[120px] flex flex-col gap-6">
-        {partyData.map((party) => (
-          <div
-            key={party}
-            className="cursor-pointer border-b border-[#F1F1F1] py-4 hover:font-bold"
-            onClick={() => {
-              handleRegionClick(party);
-            }}
-          >
-            {party}
+    <>
+      {partyStep === 0 && (
+        <section className="mb-[120px] flex w-full flex-col gap-16">
+          <div className="mb-[120px] flex flex-col gap-6">
+            {partyData.map((party) => (
+              <div
+                key={party}
+                className="cursor-pointer border-b border-[#F1F1F1] py-4 hover:font-bold"
+                onClick={() => {
+                  handleRegionClick(party);
+                }}
+              >
+                {party}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </section>
+        </section>
+      )}
+      {partyStep === 1 && <MPListContainer mpData={filteredMPData} />}
+    </>
   );
 };
-
-export default Party;
--- src/components/list/Region.tsx
+++ src/components/list/Region.tsx
@@ -1,17 +1,29 @@
 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { twMerge } from 'tailwind-merge';
+import { useEffect, useState } from 'react';
 
 import { circuitData, regionData, RegionDataType } from '@/api/regionData';
+import { MPDataType } from '@/api/api';
+import MPListContainer from '../congress/MPListContainer';
+import { cn } from '@/utils';
 
 const CURRENT_REGION = 'font-bold';
 const REGION = 'font-medium bg-[#FAFAFA] border-r border-[#F1F1F1]';
 
-export const Region = () => {
-  const router = useRouter();
+interface RegionComponentProps {
+  regionStep: number;
+  nextRegionStep: () => void;
+  mpData: MPDataType[];
+}
+
+export const RegionComponent = ({
+  regionStep,
+  nextRegionStep,
+  mpData,
+}: RegionComponentProps) => {
   const [currentCircuit, setCurrentCircuit] = useState('서울');
+  const [currentDistrict, setCurrentDistrict] = useState<string | null>(null);
+  const [filteredMPData, setFilteredMPData] = useState(mpData);
 
   const getRegionArray = (regionData: RegionDataType[], currentKey: string) => {
     const region = regionData.find((region) =>
@@ -20,52 +32,79 @@ export const Region = () => {
     return region ? region[currentKey] : [];
   };
 
-  const handleRegionClick = (currentRegion: string | null) => {
-    if (currentRegion && currentRegion.includes('전체')) {
-      return router.push(`/region/${currentCircuit}`);
-    }
-
-    return router.push(`/region/${currentCircuit}_${currentRegion}`);
+  const handleNextClick = (circuit: string) => {
+    setCurrentCircuit(circuit);
   };
 
+  const handleRegionClick = (currentRegion: string | null) => {
+    if (currentRegion && currentRegion.includes('전체')) {
+      nextRegionStep();
+    }
+    setCurrentDistrict(currentRegion);
+    nextRegionStep();
+  };
+
+  useEffect(() => {
+    const filteredData = mpData.filter((data) => {
+      if (currentDistrict) {
+        return (
+          data.constituency[0].region.includes(currentCircuit) &&
+          data.constituency[0].district?.includes(currentDistrict)
+        );
+      }
+
+      return data.constituency[0].region.includes(currentCircuit);
+    });
+
+    setFilteredMPData(filteredData);
+  }, [currentCircuit, currentDistrict, mpData]);
+
   return (
-    <section className="flex w-full gap-2.5 border-t border-[#F1F1F1] text-[16px] text-[#0E0E0E]">
-      <article className="w-auto flex-shrink-0 cursor-pointer">
-        {circuitData.map((circuit) => (
-          <div
-            className={twMerge(
-              currentCircuit === circuit ? CURRENT_REGION : REGION,
-              'border-b border-[#F1F1F1] px-6 py-4'
-            )}
-            key={circuit}
-            onClick={() => setCurrentCircuit(circuit)}
-          >
-            {circuit}
-          </div>
-        ))}
-      </article>
-      <article className="flex-grow cursor-pointer">
-        {[
-          `${currentCircuit} 전체`,
-          ...getRegionArray(regionData, currentCircuit),
-        ].map((region) => (
-          <div key={region}>
-            {region ? (
+    <>
+      {regionStep === 0 && (
+        <section className="flex w-full gap-2.5 text-[16px] text-[#0E0E0E]">
+          <article className="w-auto flex-shrink-0 cursor-pointer">
+            {circuitData.map((circuit) => (
               <div
-                key={region}
-                className="border-b border-[#F1F1F1] py-4"
+                className={cn(
+                  currentCircuit === circuit ? CURRENT_REGION : REGION,
+                  'border-b border-[#F1F1F1] px-6 py-4'
+                )}
+                key={circuit}
                 onClick={() => {
-                  handleRegionClick(region);
+                  handleNextClick(circuit);
                 }}
               >
-                {region}
+                {circuit}
               </div>
-            ) : (
-              <div />
-            )}
-          </div>
-        ))}
-      </article>
-    </section>
+            ))}
+          </article>
+          <article className="flex-grow cursor-pointer">
+            {[
+              `${currentCircuit} 전체`,
+              ...getRegionArray(regionData, currentCircuit),
+            ].map((region) => (
+              <div key={region}>
+                {region ? (
+                  <div
+                    key={region}
+                    className="border-b border-[#F1F1F1] py-4"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleRegionClick(region);
+                    }}
+                  >
+                    {region}
+                  </div>
+                ) : (
+                  <div />
+                )}
+              </div>
+            ))}
+          </article>
+        </section>
+      )}
+      {regionStep === 1 && <MPListContainer mpData={filteredMPData} />}
+    </>
   );
 };
